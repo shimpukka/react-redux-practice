f93d9f5c3962ae74676d21d60ff69a340a23d3d9
Shiori Sasaki <shioris.sk@gmail.com>
2021-12-18 19:58:23 +0200

    Updated song duration

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 
 const songsReducer = () => {
     return [
-        { title: 'Last Christmas', duration: '4:05'},
-        { title: 'Barbie girl', duration: '3:10'},
-        { title: 'Blue', duration: '2:10'},
-        { title: 'Let it go', duration: '5:45'}
+        { title: 'Last Christmas', duration: '4:22'},
+        { title: 'Barbie girl', duration: '3:17'},
+        { title: 'Blue', duration: '4:43'},
+        { title: 'Let it go', duration: '3:43'}
     ];
 };
 
